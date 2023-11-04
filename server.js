2b775c2dba1e6b9fde425458f833276adcd7188b
neeraj-sharma2592 <63321305+neeraj-sharma2592@users.noreply.github.com>
2023-11-04 20:34:51 +0530

    added Student endpoint

--- server.js
+++ server.js
@@ -7,4 +7,19 @@ app.get("/", (req, res) => {
     res.status(200).json({name: "agile-node-app"})
 });
 
+app.get("/Student", (req, res) => {
+    res.status(200).json([
+        {
+            name: "Tushar",
+            age: 23,
+            city: "Mumbai"
+        }, {
+            name: "Rajanikant",
+            age: 67,
+            city: "Chennai"
+        }
+    ])
+});
+
+
 app.listen(3000);
